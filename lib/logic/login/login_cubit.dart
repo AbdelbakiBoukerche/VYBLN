d9bc6f5350aef3f792b3d4396537c5099f7d8f15
Abdelbaki Boukerche <boukerche.abdelbaki@gmail.com>
2020-10-22 16:04:53 +0100

    BLoC: Added Login Cubit

---
+++ lib/logic/login/login_cubit.dart
@@ -0,0 +1,42 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:formz/formz.dart';
+import 'package:vybln/logic/authentication/authentication.dart';
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  final AuthenticationRepository _authenticationRepository;
+  LoginCubit(this._authenticationRepository)
+      : assert(_authenticationRepository != null),
+        super(const LoginState());
+
+  void emailChanged(String value) {
+    final email = Email.dirty(value);
+    emit(state.copyWith(
+      email: email,
+      status: Formz.validate([email, state.password]),
+    ));
+  }
+
+  void passwordChanged(String value) {
+    final password = Password.dirty(value);
+    emit(state.copyWith(
+      password: password,
+      status: Formz.validate([state.email, password]),
+    ));
+  }
+
+  Future<void> logInWithCredentials() async {
+    if (!state.status.isValidated) return;
+    emit(state.copyWith(status: FormzStatus.submissionInProgress));
+    try {
+      await _authenticationRepository.logIn(
+          email: state.email.value, password: state.password.value);
+      emit(state.copyWith(status: FormzStatus.submissionSuccess));
+    } on Exception {
+      emit(state.copyWith(status: FormzStatus.submissionFailure));
+    }
+  }
+}
