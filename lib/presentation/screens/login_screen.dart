877dd7be316caa6307ae3b77f4958a4de8c67501
Abdelbaki Boukerche <boukerche.abdelbaki@gmail.com>
2020-10-22 16:24:29 +0100

    UI: Initial login page implemented

--- lib/presentation/screens/login_screen.dart
+++ lib/presentation/screens/login_screen.dart
@@ -1,4 +1,12 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:formz/formz.dart';
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:vybln/logic/login/login_cubit.dart';
+
+import 'signup_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   static Route route() {
@@ -7,6 +15,117 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("Login");
+    return Scaffold(
+      body: BlocProvider(
+        create: (_) =>
+            LoginCubit(context.repository<AuthenticationRepository>()),
+        child: LoginForm(),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<LoginCubit, LoginState>(
+      listener: (context, state) {
+        if (state.status.isSubmissionFailure) {
+          Scaffold.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              const SnackBar(content: Text("Authentication Failed")),
+            );
+        }
+      },
+      child: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
+
+// TODO Refactor Inputs to one widget with paramaters
+class _EmailInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<LoginCubit, LoginState>(
+      buildWhen: (previous, current) => previous.email != current.email,
+      builder: (context, state) {
+        return TextField(
+          key: const Key('loginForm_emailInput_textField'),
+          onChanged: (email) => context.bloc<LoginCubit>().emailChanged(email),
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            labelText: 'Email',
+            helperText: '',
+            errorText: state.email.invalid ? 'Invalid Email' : null,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _PasswordInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<LoginCubit, LoginState>(
+      buildWhen: (previous, current) => previous.password != current.password,
+      builder: (context, state) {
+        return TextField(
+          key: const Key('loginForm_passwordInput_textField'),
+          onChanged: (password) =>
+              context.bloc<LoginCubit>().passwordChanged(password),
+          obscureText: true,
+          decoration: InputDecoration(
+            labelText: 'Password',
+            helperText: '',
+            errorText: state.password.invalid ? 'Invalid Password' : null,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _LoginButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<LoginCubit, LoginState>(
+      buildWhen: (previous, current) => previous.status != current.status,
+      builder: (context, state) {
+        if (state.status.isSubmissionInProgress) {
+          return CircularProgressIndicator();
+        } else {
+          return RaisedButton(
+            key: const Key('loginForm_login_raisedButton'),
+            child: const Text("Login"),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            color: Colors.red,
+            onPressed: state.status.isValidated
+                ? () => context.bloc<LoginCubit>().logInWithCredentials()
+                : null,
+          );
+        }
+      },
+    );
+  }
+}
+
+class _SignUpButtton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      key: const Key('loginForm_signup_flatButton'),
+      child: Text("Create Account"),
+      onPressed: () => Navigator.of(context).push<void>(SignUpScreen.route()),
+    );
   }
 }
---
+++ lib/presentation/screens/signup_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatelessWidget {
+  static Route route() {
+    return MaterialPageRoute<void>(builder: (_) => SignUpScreen());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+    );
+  }
+}
